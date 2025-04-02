7ce350107c0bd17164ba010ddff2d0f51568bf5b
JuanmaPilon <juanmartin.pilon@gmail.com>
2025-04-02 14:52:20 -0300

    adding more structure to the portfolio,like components and such

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header>
+        <h1>Hello, my name is Juan Martin Pilon</h1>
+        <h2>I'm a Software Engineer</h2>
+        </header>
+    );
+}
+
+export default Header;
