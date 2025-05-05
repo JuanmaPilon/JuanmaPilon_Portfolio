b7c3b8813d15b7ef804c750e797eb12d1c7177b5
JuanmaPilon <juanmartin.pilon@gmail.com>
2025-05-05 12:46:30 -0300

    adding light/dark mode functionality

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,31 @@
 import './Navbar.css';
+import { useEffect, useState } from 'react';
 
 const NavBar = () => {
 
-    const handleScrollToSection = (sectionId: string) => {
+    const [isDark, setIsDark] = useState(false);
+
+    useEffect(() => {
+        setIsDark(document.documentElement.classList.contains('dark'));
+    }, []);
+
+    const changeTheme = () => {
+        const html = document.documentElement;
+        html.classList.toggle('dark');
+        setIsDark(html.classList.contains('dark'));
+      };
+
+      const handleScrollToSection = (sectionId: string) => {
         const section = document.getElementById(sectionId);
         if (section) {
             section.scrollIntoView({ behavior: 'smooth' });
-
         }
     };
+    
 
 
     return (
-    <nav className="navbar bg-black/50 backdrop-blur-md text-white fixed top-0 left-0 w-full z-50">
+    <nav className="navbar bg-black/50 backdrop-blur-md text-white fixed top-0 left-0 w-full z-50 dark:bg-white/50 dark:text-white transition duration-300 ease-in-out">
         <div className='navbar-container max-w-6xl mx-auto px-4 py-4 flex justify-center space-x-6'>
             <button className='navbar-dev font-semibold hover:text-cyan-600 tracking-wide transition'
             onClick={() => handleScrollToSection("header")}>
@@ -21,6 +34,9 @@ const NavBar = () => {
             <button className='navbar-about font-semibold hover:text-cyan-600 tracking-wide transition' onClick={() => handleScrollToSection("about")}>About me</button>
             <button className='navbar-projects font-semibold hover:text-cyan-600 tracking-wide transition' onClick={() => handleScrollToSection("projects")}>Projects</button>
             <button className="navbar-contact font-semibold hover:text-cyan-600 tracking-wide transition" onClick={() => handleScrollToSection("contacts")}>Contact</button>
+            <button className='navbar-theme font-semibold hover:text-cyan-600 tracking-wide transition' onClick={() => changeTheme()}>
+                DarkB / LightB
+            </button>
         </div>
     </nav>
     );
